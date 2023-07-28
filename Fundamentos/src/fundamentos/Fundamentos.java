95eaed57166a0b43888b499b8daeecef9d4504fa
Gabriel Fonseca <gabriel.conceicao4@aluno.senai.br>
2023-07-28 10:58:15 -0300

    Declarando string como objeto

--- Fundamentos/src/fundamentos/Fundamentos.java
+++ Fundamentos/src/fundamentos/Fundamentos.java
@@ -3,11 +3,13 @@ package fundamentos;
 public class Fundamentos {
 
     public static void main(String[] args) {
-        //Declaração de variáveis
+        //Declaração de variáveis de tipos primitivos
         String nome = "Moisés";
         int idade = 40;
         boolean estudando = true;
         double media = 5.5;
+        
+        String bairro = new String("Centro");
     }
     
 }
